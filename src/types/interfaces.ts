e0d26edc07f8c259978cc12755f0bb9b6f3308b4
skyboy58568 <chisreerag@gmail.com>
2025-04-11 19:34:56 +0100

    Movie Details  page components.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -19,4 +19,13 @@ export interface BaseMovieProps {
 
   export interface BaseMovieListProps { 
     movies: BaseMovieProps[];
-  }    
+  }     
+
+  export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }
+
+
